11c061b4d3bd68aa2712ad7451f0ed15f4c36f78
Flynn <ppstream123@126.com>
2022-06-30 23:14:22 +0800

    :tada: init

---
+++ hello-ts/dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 20;
+console.log("hello world");
+if (age < 50)
+	age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ hello-ts/src/index.ts
@@ -0,0 +1,4 @@
+let age: number = 20;
+console.log("hello world");
+if (age < 50) age += 10;
+console.log(age);
